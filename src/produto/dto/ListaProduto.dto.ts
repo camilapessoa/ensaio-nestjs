5e38c9517519af7e97cbba8dded4896cceb37522
camilapessoa <camilabentespessoa@gmail.com>
2023-05-17 14:07:10 -0300

    migracoes

---
+++ src/produto/dto/ListaProduto.dto.ts
@@ -0,0 +1,19 @@
+class ListaCaracteristicaProdutoDTO {
+  nome: string;
+  descricao: string;
+}
+
+class ListaImagemProdutoDTO {
+  url: string;
+  descricao: string;
+}
+
+export class ListaProdutoDTO {
+  constructor(
+    readonly id: string,
+    readonly nome: string,
+    readonly descricao: string,
+    readonly caracteristicas: ListaCaracteristicaProdutoDTO[],
+    readonly imagens: ListaImagemProdutoDTO[],
+  ) {}
+}
